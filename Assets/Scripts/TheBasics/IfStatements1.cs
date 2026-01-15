94510586f406e91583a32e95f5921b0623b4f46e
nielspsj <nielspsj@hotmail.com>
2026-01-15 08:46:19 +0100

    updated ifstatement 1
    to its original premade state before my testing

--- Assets/Scripts/TheBasics/IfStatements1.cs
+++ Assets/Scripts/TheBasics/IfStatements1.cs
@@ -10,18 +10,9 @@ public class IfStatements1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int ballAmount = 3;
-        Debug.Log("ballAmount is now " + ballAmount);
-        Debug.Log("Another message");
-        ballAmount = 10;
-        Debug.Log(ballAmount);
-    }    
-
-    private void TestMethod()
-    {
         if (hasDungeonKey)
         {
             Debug.Log("You possess the secret key - ENTER.");
         }
-    }
+    }       
 }
